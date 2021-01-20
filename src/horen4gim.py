8fc977db41cab6b5ed2016d0d7a6dd0a7642fc3d
Bauer, Benjamin <benjamin.bauer@dlr.de>
2021-01-20 14:07:45 +0100

    Add the connection to gitlab

--- src/horen4gim.py
+++ src/horen4gim.py
@@ -1,1 +1,10 @@
-print("Hello")
+
+import gitlab
+from settings import GITLAB_URL, GITLAB_ACCESS_TOKEN
+
+if __name__ == "__main__":
+    gl = gitlab.Gitlab(GITLAB_URL, GITLAB_ACCESS_TOKEN, ssl_verify=True)
+    gl.auth()
+    projects = gl.projects.list(owned=True)
+    print(projects)
+
