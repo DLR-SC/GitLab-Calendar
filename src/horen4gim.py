332e109f912a77fa1426df605fbd826991a00079
Bauer, Benjamin <benjamin.bauer@dlr.de>
2021-01-22 09:41:50 +0100

    put all stuff from main into functions

--- src/horen4gim.py
+++ src/horen4gim.py
@@ -3,44 +3,61 @@ import gitlab
 from settings import GITLAB_URL, GITLAB_ACCESS_TOKEN, GITLAB_PROJECT_ID
 from ics import Calendar, Event
 
-# def get_project():
-# def get_issues_from_project():
-# def get_issues_from_group():
 
-# def create_event():
-
-
-def write_calendar():
-    with open('events/' + project.name + '.ics', 'w') as my_file:
-        my_file.writelines(c)
-    print("Successful creation of the file named \"", project.name, ".ics\".")
+def connect_to_gitlab():
+    gila = gitlab.Gitlab(GITLAB_URL, GITLAB_ACCESS_TOKEN, ssl_verify=True)
+    gila.auth()
+    return gila
 
 
-if __name__ == "__main__":
-    gl = gitlab.Gitlab(GITLAB_URL, GITLAB_ACCESS_TOKEN, ssl_verify=True)
-    gl.auth()
-    projects = gl.projects.list()
-    project = gl.projects.get(GITLAB_PROJECT_ID)
-    issues = project.issues.list(all=True, state='opened')
-    c = Calendar()
+def get_project():
+    proj = gl.projects.get(GITLAB_PROJECT_ID)
+    return proj
+
+
+def get_issues_from_project():
     counter = 0
-
     for issue in issues:
         issue = project.issues.get(issue.iid)
         if issue.due_date is not None:
-            e = Event()
-            e.name = 'ID: ' + str(issue.iid) + issue.title
-            e.begin = issue.due_date
-            e.description = issue.description
-            e.location = issue.web_url
-            e.make_all_day()
-            c.events.add(e)
-            print(" TITLE: ", issue.title, "\tDUE_DATE: ", issue.due_date)  # , "\tDESCRIPTION: ", issue.description)
+            create_event(issue)
             counter += 1
         else:
             continue
+    return counter
 
-    if counter != 0:
-        write_calendar()
+# def get_issues_from_group():
+
+
+def create_event(issue):
+    e = Event()
+    e.name = '[' + project.name + '] ' + issue.title + ' (' + str(issue.iid) + ')'
+    e.begin = issue.due_date
+    e.description = issue.description
+    e.location = issue.web_url
+    e.make_all_day()
+    cal.events.add(e)
+    print(" TITLE: ", issue.title, "\tDUE_DATE: ", issue.due_date)  # , "\tDESCRIPTION: ", issue.description)
+
+
+def create_calendar():
+    c = Calendar()
+    return c
+
+
+def write_calendar():
+    if issue_counter != 0:
+        with open('events/' + project.name + '.ics', 'w') as my_file:
+            my_file.writelines(cal)
+        print("Successful creation of the file named \"" + project.name + ".ics\".")
     else:
         print("There are no opened issues with a due date in the project.")
+
+
+if __name__ == "__main__":
+    gl = connect_to_gitlab()
+    project = get_project()
+    issues = project.issues.list(all=True, state='opened')
+    cal = create_calendar()
+    issue_counter = get_issues_from_project()
+    write_calendar()
