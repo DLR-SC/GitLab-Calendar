359e8f291e77cf6396e6d7f50bd2effe5d58620a
Bauer, Benjamin <benjamin.bauer@dlr.de>
2021-01-22 12:29:37 +0100

    Add function for claiming issues of a group

--- src/horen4gim.py
+++ src/horen4gim.py
@@ -1,6 +1,6 @@
 
 import gitlab
-from settings import GITLAB_URL, GITLAB_ACCESS_TOKEN, GITLAB_PROJECT_ID
+from settings import GITLAB_URL, GITLAB_ACCESS_TOKEN, GITLAB_PROJECT_ID, GITLAB_GROUP_ID
 from ics import Calendar, Event
 
 
@@ -15,23 +15,39 @@ def get_project():
     return proj
 
 
+def get_group():
+    gr = gl.groups.get(GITLAB_GROUP_ID)
+    return gr
+
+
 def get_issues_from_project():
     counter = 0
+    name = project.name
     for issue in issues:
         issue = project.issues.get(issue.iid)
         if issue.due_date is not None:
-            create_event(issue)
+            create_event(issue, name)
             counter += 1
         else:
             continue
     return counter
 
-# def get_issues_from_group():
+
+def get_issues_from_group():
+    counter = 0
+    name = group.name
+    for issue in group.issues.list():
+        if issue.due_date is not None:
+            create_event(issue, name)
+            counter += 1
+        else:
+            continue
+    return counter
 
 
-def create_event(issue):
+def create_event(issue, name):
     e = Event()
-    e.name = '[' + project.name + '] ' + issue.title + ' (' + str(issue.iid) + ')'
+    e.name = '[' + name + '] ' + issue.title + ' (' + str(issue.iid) + ')'
     e.begin = issue.due_date
     e.description = issue.description
     e.location = issue.web_url
@@ -45,19 +61,29 @@ def create_calendar():
     return c
 
 
-def write_calendar():
+def write_calendar(file_name):
     if issue_counter != 0:
-        with open('events/' + project.name + '.ics', 'w') as my_file:
+        with open('events/' + file_name + '.ics', 'w') as my_file:
             my_file.writelines(cal)
-        print("Successful creation of the file named \"" + project.name + ".ics\".")
+        print("Successful creation of the file named \"" + file_name + ".ics\".")
     else:
         print("There are no opened issues with a due date in the project.")
 
 
 if __name__ == "__main__":
     gl = connect_to_gitlab()
-    project = get_project()
-    issues = project.issues.list(all=True, state='opened')
-    cal = create_calendar()
-    issue_counter = get_issues_from_project()
-    write_calendar()
+    if GITLAB_PROJECT_ID or GITLAB_GROUP_ID is not None:
+        if GITLAB_PROJECT_ID is not None:
+            project = get_project()
+            issues = project.issues.list(all=True, state='opened')
+            cal = create_calendar()
+            issue_counter = get_issues_from_project()
+            write_calendar(project.name)
+        if GITLAB_GROUP_ID is not None:
+            group = get_group()
+            issues = group.issues.list(all=True, state='opened')
+            cal = create_calendar()
+            issue_counter = get_issues_from_group()
+            write_calendar(group.name)
+    else:
+        print("No project or group selected")
