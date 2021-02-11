e61adc7daa586b518d0779ed9748ee236efdde38
baue_bn <benjamin.bauer@dlr.de>
2021-02-11 16:56:39 +0100

    Add tests

---
+++ test/test_horen4gim.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+
+import gitlab
+import pytest
+from ics import Calendar, Event
+from gitlab.v4.objects import Project, Group
+from horen4gim import merge_events
+# from horen4gim import create_calendar, write_calendar, create_event, get_events, get_instance
+
+
+def test_merge_events():
+    e1 = Event(name="e1", location="url1")
+    e2 = Event(name="e2", location="url2")
+    e3 = Event(name="e2", location="url2")
+    expected_events = {e1, e2}
+    groups = {"g1": {e1, e2}}
+    projects = {"p1": {e3}}
+    # with pytest.raises(Exception):
+    result_events = merge_events(groups, projects)
+    assert result_events == expected_events
+
+
+def test_merge_events_empty_group():
+    e1 = Event(name="e1", location="url1")
+    e2 = Event(name="e2", location="url2")
+    e3 = Event(name="e3", location="url3")
+    expected_events = {e1, e2, e3}
+    groups = {"g1": {}}
+    projects = {"p1": {e1, e2, e3}}
+    # with pytest.raises(Exception):
+    result_events = merge_events(groups, projects)
+    assert result_events == expected_events
+
+
+def test_merge_events_empty_group_dict():
+    e1 = Event(name="e1", location="url1")
+    e2 = Event(name="e2", location="url2")
+    e3 = Event(name="e3", location="url3")
+    expected_events = {e1, e2, e3}
+    groups = {}
+    projects = {"p1": {e1, e2, e3}}
+    # with pytest.raises(Exception):
+    result_events = merge_events(groups, projects)
+    assert result_events == expected_events
+
+
+def test_merge_events_empty_project():
+    e1 = Event(name="e1", location="url1")
+    e2 = Event(name="e2", location="url2")
+    e3 = Event(name="e3", location="url3")
+    expected_events = {e1, e2, e3}
+    groups = {"g1": {e1, e2, e3}}
+    projects = {"p1": {}}
+    # with pytest.raises(Exception):
+    result_events = merge_events(groups, projects)
+    assert result_events == expected_events
+# gl = gitlab.Gitlab.from_config("dlr", config_files="gitlab-config.ini")
+
+
+# def test_create_calendar():
+#     cal = create_calendar()
+#     assert cal.creator is None
+#     assert cal.events == set()
+#
+#
+# def test_get_instance():
+#     instance_id1 = 5224
+#     instance_id2 = 10064
+#     instance1 = get_instance(gl, instance_id1)
+#     instance2 = get_instance(gl, instance_id2)
+#     assert type(instance1) is Project
+#     assert type(instance2) is Project
+#
+#
+# def test_write_calendar(tmpdir):
+#     cal = Calendar()
+#     date = datetime(2021, 1, 27, 11, 30)
+#     event = Event(name='testevent',
+#                   begin=date)
+#     filename = str(tmpdir + 'test.ics')
+#     cal.events.add(event)
+#     write_calendar(cal, filename)
+#     with open(filename, 'r') as f:
+#         content = f.readlines()
+#     assert 'testevent' in ''.join(content)
+#
+#
+# def test_get_events():
+#     instance = gl.projects.get(10064)
+#     events = get_events(instance.name, instance.issues, {"state": "opened"})
+#     assert type(events) == set
+#     assert events != ()
+#
+#
+# def test_create_event():
+#     project = gl.projects.get(10064)
+#     for issue in project.issues.list(all=True, state='opened'):
+#         if issue.due_date is not None:
+#             event = create_event(issue, project.name)
+#             assert event.location == issue.web_url
+#             assert event.description == issue.description
