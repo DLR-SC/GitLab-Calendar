6b87603b5043d4f918b07a40e5b8de8ceb134b06
baue_bn <benjamin.bauer@dlr.de>
2021-02-18 15:44:07 +0100

    add further functions

--- test/test_horen4gim.py
+++ test/test_horen4gim.py
@@ -3,16 +3,20 @@ from datetime import datetime
 import gitlab
 import pytest
 from ics import Calendar, Event
-from gitlab.v4.objects import Project, Group
-from horen4gim import merge_events, create_event
-# from horen4gim import create_calendar, write_calendar, create_event, get_events, get_instance
+from pathlib import Path
+# from gitlab.v4.objects import Project, Group
+from horen4gim import merge_events, create_event, write_calendar, write_calendars, filter_todos
 
-
-e1 = Event(name="e1", location="url1")
-e2 = Event(name="e2", location="url2")
-e3 = Event(name="e3", location="url3")
-e4 = Event(name="e4", location="url4")
-e5 = Event(name="e5same_as_e4", location="url4")
+gl = gitlab.Gitlab.from_config("dlr", config_files="gitlab-config.ini")
+e1 = Event(name="e1", location="url1", begin=datetime(2021, 1, 27, 11, 30))
+e2 = Event(name="e2", location="url2", begin=datetime(2021, 1, 27, 11, 30))
+e3 = Event(name="e3", location="url3", begin=datetime(2021, 1, 27, 11, 30))
+e4 = Event(name="e4", location="url4", begin=datetime(2021, 1, 27, 11, 30))
+e5 = Event(name="e5same_as_e4", location="url4", begin=datetime(2021, 1, 27, 11, 30))
+cal1 = Calendar(events={e1, e2})
+cal2 = Calendar(events={e3, e4})
+cal3 = Calendar()
+calendars = [(cal1, "cal1"), (cal2, "cal2"), (cal3, "cal3")]
 
 
 @pytest.mark.parametrize("groups,projects,expected_events", [
@@ -45,7 +49,7 @@ def test_merge_events_double_event_url(groups, projects, expected_events):
 
     ])
 def test_create_event(project_id, issue_id):
-    gl = gitlab.Gitlab.from_config("dlr", config_files="gitlab-config.ini")
+
     project = gl.projects.get(project_id)
     issue = project.issues.get(issue_id)
     if issue.due_date:
@@ -54,45 +58,35 @@ def test_create_event(project_id, issue_id):
         assert event.categories == {"Issues"}
         assert issue.description in event.description
 
-# def test_create_calendar():
-#     cal = create_calendar()
-#     assert cal.creator is None
-#     assert cal.events == set()
-#
-#
-# def test_get_instance():
-#     instance_id1 = 5224
-#     instance_id2 = 10064
-#     instance1 = get_instance(gl, instance_id1)
-#     instance2 = get_instance(gl, instance_id2)
-#     assert type(instance1) is Project
-#     assert type(instance2) is Project
-#
-#
-# def test_write_calendar(tmpdir):
-#     cal = Calendar()
-#     date = datetime(2021, 1, 27, 11, 30)
-#     event = Event(name='testevent',
-#                   begin=date)
-#     filename = str(tmpdir + 'test.ics')
-#     cal.events.add(event)
-#     write_calendar(cal, filename)
-#     with open(filename, 'r') as f:
-#         content = f.readlines()
-#     assert 'testevent' in ''.join(content)
-#
-#
-# def test_get_events():
-#     instance = gl.projects.get(10064)
-#     events = get_events(instance.name, instance.issues, {"state": "opened"})
-#     assert type(events) == set
-#     assert events != ()
-#
-#
-# def test_create_event():
-#     project = gl.projects.get(10064)
-#     for issue in project.issues.list(all=True, state='opened'):
-#         if issue.due_date is not None:
-#             event = create_event(issue, project.name)
-#             assert event.location == issue.web_url
-#             assert event.description == issue.description
+
+def test_write_calendar(tmpdir):
+    cal = Calendar()
+    date = datetime(2021, 1, 27, 11, 30)
+    event = Event(name='testevent',
+                  begin=date)
+    filename = str(tmpdir + 'test.ics')
+    cal.events.add(event)
+    write_calendar(cal, filename, "test.ics")
+    with open(filename, 'r') as f:
+        content = f.readlines()
+    assert 'testevent' in ''.join(content)
+
+
+@pytest.mark.parametrize("cals", [
+    calendars
+])
+def test_write_calendars(tmpdir, cals):
+    p = tmpdir.mkdir("test")
+    write_calendars(calendars, Path(p))
+    with open(str(p + "/cal1.ics"), 'r') as f:
+        content = f.readlines()
+    assert 'e1' in ''.join(content)
+    assert 'LOCATION:url1' in ''.join(content)
+    assert Path(str(p + "/cal3.ics")).exists() is False
+
+
+def test_filter_todos_from_project():
+    issue_events, milestone_events = filter_todos(gl.projects.get(10064),
+                                                  True, True)
+    assert issue_events != set()
+    assert milestone_events != set()
