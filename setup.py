6f7b266a975b2d41e7be0018884cf8d4b91ec059
baue_bn <benjamin.bauer@dlr.de>
2021-03-11 09:49:46 +0100

    Add further specifications

--- setup.py
+++ setup.py
@@ -6,19 +6,21 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
+
 package = setuptools.find_packages()
+
 setuptools.setup(
     name="gitcalendar",
     version="0.1",
     author="Benjamin Moritz Bauer",
     author_email="benjamin.bauer@dlr.de",
+    maintainer="Deutsches Zentrum fuer Luft- und Raumfahrt e.V. (DLR)",
     description="Tool that generates ics-files from a repositories issues,"
                 " milestones and iterations, which have a due date.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
-    license='MIT',
-
+    url="https://github.com/DLR-SC/gitcalendar",
+    license='License :: MIT',
     packages=package,
     entry_points={
         'console_scripts': [
@@ -26,6 +28,7 @@ setuptools.setup(
         ],
     },
     classifiers=[
+        "Development Status :: 1 - Alpha"
         "Programming Language :: Python :: 3.9",
 
     ],
